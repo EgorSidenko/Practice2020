e6045c66deaef6cd0cde874efc47021c62a61160
EgorSidenko <yegor.sidenko@gmail.com>
2020-06-18 09:07:00 +0300

    Add for, function

--- ChessKing.c
+++ ChessKing.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main (void)
+#include <string.h>
+#define rows 8
+#define cols 8 
+int OutputBoard()
 {
-    printf("dasda");
+    int board[rows][cols], i, j, k, count=1;
+    k = 1;
+
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=cols; j++)
+        {
+            if(k == 1)
+            {
+                printf("▒");
+            }
+            else
+            {
+                 printf("▓");
+            }
+
+            // If k = 1  then k *= -1 => -1
+            // If k = -1 then k *= -1 =>  1
+            k *= -1;
+            board[i][j] = 1 * count++;  
+        }
+        if(cols % 2 == 0)
+        {
+            k *= -1;
+        }
+        printf("\n");
+    }
+}
+
+void KingSet (){
+
+}
+
+int main()
+{
+    int board[rows][cols];
+    int king[rows][cols];
+    OutputBoard(); //♕
+    KingSet();
+ for (;;) {
+
+ }
+ 
     return 0;
 }
